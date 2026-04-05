41152a833d551eaf9525fd6a131c0b0d2dbbad93
Emir Dzhalalov <dzalalovemran@gmail.com>
2026-04-05 04:04:55 +0300

    fix: remove duplicate Toaster from main.tsx

--- miniapp/src/main.tsx
+++ miniapp/src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Toaster } from 'react-hot-toast'
+
 import App from './App'
 import './index.css'
 
@@ -20,18 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          duration: 3000,
-          style: {
-            background: 'var(--bg2)',
-            color: 'var(--text)',
-            borderRadius: '14px',
-            fontSize: '14px',
-          },
-        }}
-      />
     </QueryClientProvider>
   </React.StrictMode>
 )
