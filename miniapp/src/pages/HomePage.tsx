a2aa0c8e68cd9105009050d4d09cfd44b62dd25c
Emir Dzhalalov <dzalalovemran@gmail.com>
2026-04-19 19:37:23 +0300

    fix: remove stray unclosed div in HomePage trending section

--- miniapp/src/pages/HomePage.tsx
+++ miniapp/src/pages/HomePage.tsx
@@ -689,7 +689,6 @@ export default function HomePage() {
             Популярное
           </h2>
 
-          <div>
           <div
             ref={trendingScrollRef}
             onScroll={() => {
